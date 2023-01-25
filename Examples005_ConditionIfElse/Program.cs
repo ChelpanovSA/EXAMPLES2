76714e75aa5a8f133c86060c73317ae66cab3293
Сергей Челпанов <S.inter.4@mail.ru>
2023-01-25 12:34:02 +0300

    add Examples005_ConditionIfElse

---
+++ Examples005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Урряя это же Манька!!! ");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
